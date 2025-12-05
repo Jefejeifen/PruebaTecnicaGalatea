54caa184791b5c4dd8066eb8441cbc11cda3b7d2
Jefejeifen <dylanmateollano@gmail.com>
2025-12-04 20:41:28 -0500

    fix: change imports into controller

--- src/main/java/com/challenge/elowen/artifact_clue_detector/controller/ClueController.java
+++ src/main/java/com/challenge/elowen/artifact_clue_detector/controller/ClueController.java
@@ -4,7 +4,10 @@ import com.challenge.elowen.artifact_clue_detector.model.Manuscript;
 import com.challenge.elowen.artifact_clue_detector.service.ArtifactClueService;
 import com.challenge.elowen.artifact_clue_detector.util.ClueResponseBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
