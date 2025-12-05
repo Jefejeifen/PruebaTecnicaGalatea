e8cd82db39d4c966652c91a2a5ec3a6f7973183a
Jefejeifen <dylanmateollano@gmail.com>
2025-12-04 19:56:31 -0500

    fix: delete command line runner

--- src/main/java/com/challenge/elowen/artifact_clue_detector/ArtifactClueDetectorApplication.java
+++ src/main/java/com/challenge/elowen/artifact_clue_detector/ArtifactClueDetectorApplication.java
@@ -7,35 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Clase principal de Spring Boot.
- * Nivel 1: Se usa CommandLineRunner para ejecutar el análisis al iniciar.
  */
 @SpringBootApplication
-public class ArtifactClueDetectorApplication implements CommandLineRunner {
-
-	private final ArtifactClueDetector detector;
-
-	public ArtifactClueDetectorApplication(ArtifactClueDetector detector) {
-		this.detector = detector;
-	}
+public class ArtifactClueDetectorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArtifactClueDetectorApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) {
-		String[] manuscript = {
-				"RTHGQW",
-				"XRLQRE",
-				"NARURR",
-				"REVRAL",
-				"EGSILE",
-				"BRINDS"
-		};
-
-		boolean hasClue = detector.containsArtifactClue(manuscript);
-		System.out.println("¿Contiene pista de artefacto? " + hasClue);
-		System.out.println(detector.getFoundClueDescription());
 	}
 
 }
